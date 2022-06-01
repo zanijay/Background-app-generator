78e39e1918afa400fc65d26de830ff93a68e5871
zanijay <zangahmalik16@yahoo.com>
2022-05-31 20:05:55 -0500

    release

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var _ = require('lodash');
+
+console.log(_);
+
+var array = [1,2,3,4,5,6,7,8];
+
+console.log('answer',_.without(array,4));
+
 var css = document.querySelector("h3");
 var col1 = document.querySelector(".color1");
 //using . class selector like in CSS
